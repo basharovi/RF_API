d92888152249fc7422a67c2ce07352e0b4d019c6
Bashar Ovi <mominur.ovi@savethechildren.org>
2021-04-11 11:56:09 +0600

    added product info model

---
+++ BusinessDomain/Aggregates/Product/ProductInfo.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using RapidFireLib.Lib.Core;
+using System.ComponentModel.DataAnnotations;
+
+namespace BusinessDomain.Aggregates.Product
+{
+    public class ProductInfo : IModel
+    {
+        [Key]
+        public string ProductId { get; set; } = Guid.NewGuid().ToString();
+        public string ProductName { get; set; }
+        public decimal Price { get; set; }
+    }
+}
