0f2a4e3165b2578fb0d9e8020c27cab36012432e
Bashar Ovi <mominur.ovi@savethechildren.org>
2021-04-11 11:38:50 +0600

    Added Swagger

--- Api/Startup.cs
+++ Api/Startup.cs
@@ -8,6 +8,7 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.IdentityModel.Tokens;
 using Newtonsoft.Json.Serialization;
 using RapidFireLib;
+using Swashbuckle.AspNetCore.Swagger;
 using System.Text;
 
 namespace API
@@ -24,6 +25,10 @@ namespace API
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddSwaggerGen(c => {
+                c.SwaggerDoc("v1", new Info { Title = "Employee API", Version = "V1" });
+            });
+
             services.AddRapidFire(new AppConfig());
 
             services.AddCors(options =>
@@ -76,6 +81,10 @@ namespace API
             app.UseCors();
             app.UseAuthentication();
             app.UseHttpsRedirection();
+            app.UseSwagger();
+            app.UseSwaggerUI(c => {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "post API V1");
+            });
             app.UseMvc(routes =>
             {
                 routes.MapRoute("Default", "api/{controller}/{action}/{id?}");
